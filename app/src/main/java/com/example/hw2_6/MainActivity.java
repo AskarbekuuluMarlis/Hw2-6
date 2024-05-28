ecfd256ab14e935d4db32ec88e14419dc46a99ca
AskarbekuuluMarlis <marlisaskarbekov00000@gmail.com>
2024-05-28 16:59:47 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw2_6/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.hw2_6;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText email;
+    private EditText password;
+    private Button btn;
+    private ConstraintLayout container;
+    private TextView log_in;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btn = findViewById(R.id.log_in2);
+        container = findViewById(R.id.container);
+        log_in = findViewById(R.id.log_in);
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP){
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        btn.setOnClickListener(view -> {
+            if (email.getText().toString().equals("admin") &&
+                    password.getText().toString().equals("admin")){
+                container.setVisibility(View.GONE);
+                log_in.setVisibility(View.GONE);
+                Toast.makeText(MainActivity.this, "Вы успешно зарегистрировались", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Неправильный логин и пароль", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (email.getText().toString().length() >= 1){
+                    btn.setBackgroundColor(btn.getContext().getResources().getColor(R.color.brown));
+                }else{
+                    btn.setBackgroundColor(btn.getContext().getResources().getColor(R.color.grey));
+                }
+            }
+        });
+
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (password.getText().toString().length() >= 1){
+                    btn.setBackgroundColor(btn.getContext().getResources().getColor(R.color.brown));
+                }else{
+                    btn.setBackgroundColor(btn.getContext().getResources().getColor(R.color.grey));
+                }
+            }
+        });
+    }
+}
